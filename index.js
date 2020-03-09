b6ef4011bbf8984af6086225f1b261fd68a7613f
Adrian Hodrea <adihodrea@yahoo.com>
2020-03-09 17:27:35 +0200

    orice

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 document.querySelector('button').addEventListener('click', function () {
-  let event = this.window;
-  const newItem = document.createElement("li");
-  const textNode = document.createTextNode("Water");
+  const newItem = document.createElement('li');
+  const textNode = document.createTextNode('Water');
   newItem.appendChild(textNode);
   document.querySelector('ul').insertBefore(newItem, document.querySelector('ul').childNodes[0])
-    console.log("works!!!");
 });
