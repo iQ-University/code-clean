b5b7aa206d433df20934c7fb2121efebb39c02f7
Andrei Mateescu <Andrei.Mateescu@verizonmedia.com>
2020-03-09 13:43:21 +0200

    modified

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-var response = 0;
-function add(arr) {
+var sumArrAy = 0;
+
+
+function addArrayFinalSum(arr) {
   for (let index = 0, len = arr.length; index < len; index++) {
+
     if (typeof arr[index] !== 'number') {
-      response = response + add(arr[index])
+      sumArrAy = sumArrAy + addArrayFinalSum(arr[index])
+
     } else {
-      response = response + arr[index]
+
+      sumArrAy = sumArrAy + arr[index]
     }
   }
+  response = sumArrAy;
+
   return response;
+  console.log("got here!!!");
 }
-console.log(add([1, [2, [3, 4], [5, 6, 7, 8], 9, 10]]));
+console.log('abc:', addArrayFinalSum([1,2,3,4,5,6]));
