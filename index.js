60619a47826a9cebd45c6375e49a11db43a3f7c6
Andrei Mateescu <Andrei.Mateescu@verizonmedia.com>
2020-03-09 13:50:09 +0200

    modified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.querySelector('button').addEventListener('click', function () {
+  let event = this.window;
   var NewItem = document.createElement("LI");
-  var textnode = document.createTextNode("Water");
+   var textnode = document.createTextNode("wateR");
     NewItem.appendChild(textnode);
     document.querySelector('ul').insertBefore(NewItem, document.querySelector('ul').childNodes[0])
+    console.log("works!!!");
 });
