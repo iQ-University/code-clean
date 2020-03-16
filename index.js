8f3e60b3f73e2aa6ddefa56699f7433e65622f5b
PopicaMihai <popmihai2707gmail.com>
2020-03-16 03:08:14 -0700

    Problema 1

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-let CLEANROOM = function () {
-  return new Promise(function(resolve, reject) {
-    resolve('Cleaned the room');
-  });
-}
-
-
-let removegarbage = function (message) {
-  return new Promise(function(resolve, reject) {
-    resolve(`${message} Removed garbage`);
-  });
-}
-
-let WinIcecream = function (msgg) {
-  return new Promise(function(resolve, reject) {
-    resolve(`${msgg} Won Icecream`);
-  });
-}
-
-
-
-CLEANROOM().then(function(res) {
-  removegarbage(res).then(function(res){
-  WinIcecream(res).then(function(resP){
-    console.log('abcdefg', resP)
-    })
+const cleanRoom = () => {
+    return new Promise((res) => {
+      res('Cleaned the room');
+    });
+  }
+  
+  const removeGarbage = (mes) => {
+    return new Promise((res) => {
+      res(`${mes} Removed garbage`);
+    });
+  }
+  
+  const winIcecream = (msg) => {
+    return new Promise((res) => {
+      res(`${msg} Won Icecream`);
+    });
+  }
+  
+  cleanRoom().then((res) => {
+    removeGarbage(res).then((res) => {
+      winIcecream(res).then((resP) => {
+        console.log(`abcdefg  ${resP}`);
+      });
+    });
   })
-})
